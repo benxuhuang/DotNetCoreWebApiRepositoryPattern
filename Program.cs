59468f60fb465dc2a44af8eba8cc1c63eb4b5dfe
Ben Huang <benxu.huang@gmail.com>
2021-12-27 16:21:26 +0800

    add sql server connection string

--- Program.cs
+++ Program.cs
@@ -1,12 +1,16 @@
 using DotNetCoreWebApiRepositoryPattern.Data.EFCore;
+using DotNetCoreWebApiRepositoryPattern.Models.EFCore;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<MyDBContext>(options =>
+    options.UseSqlServer("Server=localhost\\SQLEXPRESS01;Database=MyDB;Trusted_Connection=True;MultipleActiveResultSets=true"));
+
 // Add services to the container.
+builder.Services.AddScoped<EfCoreMovieRepository>();
 
 builder.Services.AddControllers();
-
-builder.Services.AddScoped<EfCoreMovieRepository>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
